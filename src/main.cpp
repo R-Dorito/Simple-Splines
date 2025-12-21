9d432bc409b51147972683a6c8a92b9421394d96
R_Dorito <quilt-cane-kept@duck.com>
2025-12-21 15:24:30 +1100

    commit before it fully breaks

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,27 @@ typedef struct  Ball {
     Color colour;
 } Ball;
 
+void drawLinearInterprolation(Vector2 v1, Vector2 v2){
+    
+    float pointY;
+    for (float i = v1.x; i <= v2.x; i += 0.1f)
+    {
+        pointY = v1.y + (i - v1.x) * (v2.y - v1.y)/(v2.x - v1.x);
+        
+        DrawLine(v1.x + i, pointY, v2.x, v2.y, BLACK);
+        std::cout << "v1.x " << v1.x << " v1.y " << v1.y << std::endl;
+        std::cout << " v2.x " << v2.x << " v2.y " << v2.y << std::endl;
+    }
+}
 
 void drawExistingBalls(Ball balls[], int ballNum){
     for (int i = 0; i < ballNum; i++)
     {
         if(i > 0)
         {
-            DrawLineV(balls[i-1].pos, balls[i].pos, BLACK);
-            std::cout << "ball first " << i-1 << " and second " << i << std::endl;        
+            //DrawLineV(balls[i-1].pos, balls[i].pos, BLACK);
+            //std::cout << "ball first " << i-1 << " and second " << i << std::endl;
+            drawLinearInterprolation(balls[i-1].pos, balls[i].pos);        
         }
         DrawCircleV(balls[i].pos, balls[i].radius, balls[i].colour);
     }
@@ -58,10 +71,10 @@ int main(void)
                 BLUE
             };
             ballNum++;
-            std::cout << "Num balls: " << ballNum << 
-                " position x: "<< ballPosition.x <<
-                " position y: "<< ballPosition.y <<
-            std::endl;
+            // std::cout << "Num balls: " << ballNum << 
+            //     " position x: "<< ballPosition.x <<
+            //     " position y: "<< ballPosition.y <<
+            // std::endl;
         }    
     
         BeginDrawing();
