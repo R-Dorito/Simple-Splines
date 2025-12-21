d921458a31540a8c152d3501d81cfadf2c086eeb
R_Dorito <quilt-cane-kept@duck.com>
2025-12-21 21:18:13 +1100

    something broke in my recursion.

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,11 @@ float factorio(int n){
     return n * factorio(n - 1);
 }
 
-void drawLinearInterprolation(Vector2 v1, Vector2 v2){
+float binomials(int n, int i){
+    return factorio(n) / (factorio(i) * factorio(n-i));
+}
 
+void drawLinearInterprolation(Vector2 v1, Vector2 v2){
     //float pointY;
     for (float i = 0; i < 1; i += 0.01f)
     {
@@ -27,18 +30,42 @@ void drawLinearInterprolation(Vector2 v1, Vector2 v2){
     }
 }
 
-void drawBezier(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4){
+float calcsTestX(Ball balls[], int n, int i, int t){
+    float bio = binomials(n,i);
+    float t_Pain = pow(t,i);
+    float i_t = 0.0f;
+
+    if(n > 0){i_t = pow((1-t), n-1);}
+    else {i_t = 1;}
+
+    return bio * t_Pain * i_t * balls[i].pos.x + calcsTestX(balls, n-1, i++, t);
+}
+
+float calcsTestY(Ball balls[], int n, int i, float t){
+    float bio = binomials(n,i);
+    float t_Pain = (float)pow(t,i);
+    float i_t = 0.0f;
+
+    if(n > 0){i_t = (float)pow((1-t), n-1);}
+    else {i_t = 1;}
+
+    return bio * t_Pain * i_t * balls[i].pos.y + calcsTestY(balls, n-1, i++, t);
+}
+
+void drawBezier(Ball balls[], int ballCount){
     Vector2 point;
 
     for (float t = 0; t < 1.0f; t += 0.005f){
-        point.x = (1 - t) * (1 - t) * (1 - t) * p1.x 
-            + 3 * (1 - t) * (1 - t) * t * p2.x 
-            + 3 * (1 - t) * t * t * p3.x 
-            + t * t * t * p4.x;
-        point.y = (1 - t) * (1 - t) * (1 - t) * p1.y 
-            + 3 * (1 - t) * (1 - t) * t * p2.y 
-            + 3 * (1 - t) * t * t * p3.y 
-            + t * t * t * p4.y;
+        // point.x = (1 - t) * (1 - t) * (1 - t) * p1.x 
+        //     + 3 * (1 - t) * (1 - t) * t * p2.x 
+        //     + 3 * (1 - t) * t * t * p3.x 
+        //     + t * t * t * p4.x;
+        // point.y = (1 - t) * (1 - t) * (1 - t) * p1.y 
+        //     + 3 * (1 - t) * (1 - t) * t * p2.y 
+        //     + 3 * (1 - t) * t * t * p3.y 
+        //     + t * t * t * p4.y;
+        point.x = calcsTestX(balls, ballCount, 0, t);
+        point.y = calcsTestX(balls, ballCount, 0, t);
         DrawCircleV(point, 2, RED); 
     }
 }
@@ -47,10 +74,9 @@ void drawExistingBalls(Ball balls[], int ballNum){
     for (int i = 0; i < ballNum; i++)
     {
 
-        if(ballNum > 3)
-        {
-            drawBezier(balls[0].pos, balls[1].pos, balls[2].pos, balls[3].pos);
-        }
+        
+        drawBezier(balls, ballNum);
+        
         DrawCircleV(balls[i].pos, balls[i].radius, balls[i].colour);
         if(i > 0){
             //drawLinearInterprolation(balls[i-1].pos, balls[i].pos);
@@ -101,7 +127,7 @@ int main(void)
 
             ClearBackground(RAYWHITE);
             drawExistingBalls(balls, ballNum);
-            
+
             //below is the current ball location
             DrawCircleV(ballPosition, startBallRadius, LIME);
 
