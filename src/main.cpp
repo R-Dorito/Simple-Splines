b3861a7587219c1d8c4859765d6c5f1fd4ea2290
R_Dorito <quilt-cane-kept@duck.com>
2025-12-21 19:43:50 +1100

    things are working, but only for a 4 point Bezier, I want to create an infinite amount  (maybe not infinite) of points.. I need to use a generic equation to determine the total number of points and the formula used to calculate all points.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@ typedef struct  Ball {
     Color colour;
 } Ball;
 
+float factorio(int n){
+    if (n <= 1){return 1;}
+    return n * factorio(n - 1);
+}
+
 void drawLinearInterprolation(Vector2 v1, Vector2 v2){
 
     //float pointY;
@@ -22,52 +27,34 @@ void drawLinearInterprolation(Vector2 v1, Vector2 v2){
     }
 }
 
-float powerOf(float base, int n){
-    float ans = 1;
-    for(int i = 0; i < n; i++){
-        ans *= base;
-    }
-    return ans;
-}
-
 void drawBezier(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4){
-    float pointX;
-    float pointY;
+    Vector2 point;
+
     for (float t = 0; t < 1.0f; t += 0.005f){
-        // pointX = 
-        //     powerOf((1 - t),3) * p1.x +
-        //     3 * t * powerOf((1-t),2) * p2.x +
-        //     3 * powerOf(t,2) * (1-t) * p3.x +
-        //     powerOf(t,4);
-
-        // pointY = 
-        //     powerOf((1 - t),3) * p1.y +
-        //     3 * t * powerOf((1-t),2) * p2.y +
-        //     3 * powerOf(t,2) * (1-t) * p3.y +
-        //     powerOf(t,4);
-
-        pointX = (1 - t) * (1 - t) * (1 - t) * p1.x 
+        point.x = (1 - t) * (1 - t) * (1 - t) * p1.x 
             + 3 * (1 - t) * (1 - t) * t * p2.x 
             + 3 * (1 - t) * t * t * p3.x 
             + t * t * t * p4.x;
-        pointY = (1 - t) * (1 - t) * (1 - t) * p1.y 
+        point.y = (1 - t) * (1 - t) * (1 - t) * p1.y 
             + 3 * (1 - t) * (1 - t) * t * p2.y 
             + 3 * (1 - t) * t * t * p3.y 
             + t * t * t * p4.y;
-        DrawPixel((int)pointX, (int)pointY, BLACK);   
+        DrawCircleV(point, 2, RED); 
     }
 }
 
 void drawExistingBalls(Ball balls[], int ballNum){
     for (int i = 0; i < ballNum; i++)
     {
-        if((ballNum) % 4 == 0)
+
+        if(ballNum > 3)
         {
             drawBezier(balls[0].pos, balls[1].pos, balls[2].pos, balls[3].pos);
         }
         DrawCircleV(balls[i].pos, balls[i].radius, balls[i].colour);
         if(i > 0){
-            drawLinearInterprolation(balls[i-1].pos, balls[i].pos);
+            //drawLinearInterprolation(balls[i-1].pos, balls[i].pos);
+            DrawLineEx(balls[i-1].pos, balls[i].pos, 2, BLUE);
         }
     }
 }
@@ -82,7 +69,7 @@ int main(void)
 
     int ballNum = 0;    
     Ball balls[MAX_BALLS]; 
-    float startBallRadius = 5;
+    float startBallRadius = 10;
     
     Vector2 ballPosition = {
         (float) screenWidth / 2,
@@ -108,17 +95,15 @@ int main(void)
                 BLUE
             };
             ballNum++;
-            // std::cout << "Num balls: " << ballNum << 
-            //     " position x: "<< ballPosition.x <<
-            //     " position y: "<< ballPosition.y <<
-            // std::endl;
         }    
     
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
             drawExistingBalls(balls, ballNum);
-            DrawCircleV(ballPosition, startBallRadius, BLUE);
+            
+            //below is the current ball location
+            DrawCircleV(ballPosition, startBallRadius, LIME);
 
         EndDrawing();
 
